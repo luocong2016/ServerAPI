d4942b468114dda643b4ab0faeefcf9ecf50b147
luocong2016 <695393722@qq.com>
2017-09-24 17:23:21 +0800

    init

--- routes/course.js
+++ routes/course.js
@@ -9,16 +9,33 @@ const router = express.Router();
 
 const { operation } = require('../db/mysqlOperation');
 
-router.post('/getCourse', function(req, res, next) {
-    const { limit = 5 } = req.body;
+const limi_5 = 5;
+const limi_10 = 10;
 
-    let response = {status: false}
-    let result = operation('SELECT * FROM `course` ORDER BY `createtime` DESC LIMIT 5');
+
+router.post('/getCourse', function(req, res, next) {
+    let { limitStart = '', limitEnd = ''} = req.body;
+    let limit = ` LIMIT ${limi_10}`;
+    limitStart = parseInt(limitStart);
+    limitEnd = parseInt(limitEnd);
+    if(!isNaN(limitStart)){
+        if(!isNaN(limitEnd) && limitStart < limitEnd){
+            limit = ` LIMIT ${limitStart},${limitEnd}`;
+        }else{
+            limit = ` LIMIT ${limitStart}`;
+        }
+    }
+    const response = {status: false}
+    const sql = "SELECT *,DATE_FORMAT(createtime,'%Y-%m-%d %k:%i:%s') AS `createtime`,DATE_FORMAT(updatetime,'%Y-%m-%d %k:%i:%s') AS `updatetime` FROM `course` ORDER BY `createtime` DESC"
+    const result = operation(sql + limit);
     result.then(function(data){
         response.status = true;
         response.data = data;
         res.send(JSON.stringify(response));
-    })
+    }).catch(function(err){
+        response.message = err;
+        res.send(JSON.stringify(response));
+    });
 })
 
 module.exports = router;
